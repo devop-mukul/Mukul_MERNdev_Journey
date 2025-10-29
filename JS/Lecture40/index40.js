bf9e4a51a1a6a4ab775614337e700e9ffc8df355
Mukul <mails2mukull@gmail.com>
2025-10-29 20:32:09 +0530

    datatypes

---
+++ JS/Lecture40/index40.js
@@ -0,0 +1,33 @@
+// var age = 25;
+
+// if(true) {
+//     console.log(age);
+// }
+
+// function solve() {
+//     var age = 24;
+//     console.log(age);   
+// }
+// console.log(age);
+// solve();
+
+// {
+//     let a = 10;
+// }
+// console.log(a);
+
+// let a = 10;
+// a = "babbar"
+// a = true
+// a = null
+
+// console.log(a);
+
+// const a = "hello";
+// console.log(a);
+
+// a = 10;
+
+const bigNumber = 12345676543234567654323456756789098929229292929292992929765567898765n;
+
+console.log(typeof(bigNumber));
