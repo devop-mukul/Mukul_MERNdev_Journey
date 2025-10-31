fb28fe6c22d02e7451f3eb9d73872a187c765734
Mukul <mails2mukull@gmail.com>
2025-10-31 15:01:52 +0530

    Operators and Conditionals

---
+++ JS/Lecture41/conditionals.js
@@ -0,0 +1,20 @@
+// let age = 21;
+
+// if(age < 18) {
+//     console.log("Underage");
+// }
+// else if(age > 20) {
+//     console.log("AADMI HAI BHAI");
+// }
+// else if(age >= 18) {
+//     console.log("Eligible to vote!")
+// }
+
+let num = 3;
+
+switch(num) {
+    case 1: console.log("A"); break;
+    case 2: console.log("B"); break;
+    case 3: console.log("B"); break;
+    default: console.log("F");
+}
---
+++ JS/Lecture41/operators.js
@@ -0,0 +1,12 @@
+let a = 10;
+console.log(++a);
+console.log(a);
+
+console.log('5' === 5);
+console.log('5' !== 5);
+
+console.log(2 ^ 2);
+console.log(2 | 5);
+ 
+console.log(1 << 4); //1 left shift 4 times
+console.log(4 >> 1); //4 right shift 1 time
