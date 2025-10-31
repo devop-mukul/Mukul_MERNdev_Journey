f77776a4f48f9c77c4efce32a6961e5d45e33d21
Mukul <mails2mukull@gmail.com>
2025-10-31 17:42:52 +0530

    functions and arrow functions

---
+++ JS/Lecture43/functionsAndArrow.js
@@ -0,0 +1,40 @@
+//function definition
+
+// function saymyname() {
+// console.log("Mukul Kumar");
+// }
+
+// function use kaise krenge? - function call ke through
+
+// saymyname();
+
+// function printCount() {
+//     for(let i = 1; i < 100; i++) {
+//         console.log(i);
+//     }
+// }
+
+// printCount();
+
+// function getsum(a, b, c) {
+//     let sum = a + b + c;
+//     return sum;
+// }
+
+// let ans = getsum(1, 2, 3);
+// console.log("printing sum:", ans);
+
+// let getSum = function(a, b) {
+//     return a + b;
+// }
+
+// let ans = getsum(2, 3);
+// console.log(ans);
+
+//ARROW FUNCTION
+let getsum = (a, b) => {
+    return a + b;
+}
+
+let ans = getsum(2, 3);
+console.log(ans);
