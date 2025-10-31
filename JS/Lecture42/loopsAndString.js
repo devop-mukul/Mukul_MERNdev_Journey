a7462a5755885896cf26857023c449060fb07233
Mukul <mails2mukull@gmail.com>
2025-10-31 16:45:51 +0530

    loos and strings

---
+++ JS/Lecture42/loopsAndString.js
@@ -0,0 +1,50 @@
+// let i = 1;
+
+// while(i < 6) {
+//     console.log(i);
+//     i++;
+// }
+
+// for(let i = 1; i < 6; i++) {
+//     console.log(i);
+// }
+
+// let i = 1
+// do{
+// console.log(i);
+// //updation
+// i++;
+// }while(i<6);
+
+// let fname = "Mukul"; // stores string as primitive value
+// console.log(fname);
+
+// let fname = `This
+// is
+// my
+// area. `;
+
+// console.log(fname);
+
+// let firstname = new String("Mukul Kumar"); //stores string as object
+// console.log(firstname);
+
+// let op1 = "English";
+// let op2 = "Hindi";
+// // let finalAns = `${op1}${op2}';
+
+
+// let ans = op1 + op2;
+// console.log(ans);
+// console.log(op1.toUpperCase());
+// console.log(op2.toLowerCase());
+
+// let str = "Babbar";
+// console.log(str.substring(2,4));
+// console.log(str.substring(2));
+
+let sentence = "Hello Mukul ji and Kaushani Ji";
+
+let words = sentence.split(" ");
+console.log(words);
+console.log(words.join(","));
