2487180c0637e2f52f8b688f619a376fc7debf9d
Mukul <mails2mukull@gmail.com>
2025-12-16 01:11:42 +0530

    Built-In Objects(Math, Date)

---
+++ JS/Lecture48_Builtin_Objects/l48.js
@@ -0,0 +1,23 @@
+// console.log("Built-In Objects");
+
+// console.log(Math.PI);
+
+// console.log(Math.max(10, -1, 3, 100));
+
+// console.log(Math.min(10, 20, 4, -1));
+
+// console.log(Math.floor(Math.random()*10));
+
+// console.log(Math.floor(1.8));
+
+// console.log(Math.ceil(1.8));
+
+// console.log(Math.round(1.9));
+
+
+let curr = new Date();
+
+console.log(curr);
+console.log(curr.getFullYear());  
+console.log(curr.getMonth());  
+console.log(curr.getDay());  
