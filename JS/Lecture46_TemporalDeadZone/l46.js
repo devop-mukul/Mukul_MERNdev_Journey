2687817f19561d1e9e772d8c0c5867a637c82f46
Mukul <mails2mukull@gmail.com>
2025-12-16 00:27:34 +0530

    temporal dead zone

---
+++ JS/Lecture46_TemporalDeadZone/l46.js
@@ -0,0 +1,69 @@
+//temporal dead zone
+
+// console.log(age);
+console.log("Mukul");
+console.log("Kumar");
+
+let age = 2;
+
+console.log(age)
+
+
+
+
+
+
+//block scope
+
+// console.log(age);//undefined because only declaration gets hoisted not initialization
+// console.log(fn);//gives reference error
+// { 
+//     var age = 12;
+//     let fn = "Mukul";//let and const are block scoped
+// }
+// console.log(fn);//gives reference error
+// console.log(age);//works correct because var is not block scope
+
+//function scope
+
+// function say() {
+//     var fname = "Mukul";
+//     console.log("hello", fname);
+// }
+
+// console.log(fname); //will give reference error
+
+
+
+
+//global scope
+
+// var age = 25;
+
+// console.log(age);
+
+// if(true) {
+//     console.log(age);
+// }
+
+// for(let i = 0; i < 2; i++) {
+//     console.log(age);
+// }
+
+// function sayHello() {
+//     console.log(age);
+// }
+
+// sayHello();
+
+
+
+
+
+
+
+
+
+
+
+
